79ed05559bf20d99c672cd446cacf5dd1d56cacb
kai116 <55518563+kai116@users.noreply.github.com>
2020-01-07 02:25:18 +0800

    VR期末

---
+++ Unity_VR_B1040410_kai/Assets/game.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour {
+    public bool shot=false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void freeze()
+    {
+        if (shot == true)
+        {
+            this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            shot = false;
+        }
+    }
+    public void TOyz()
+    {
+        this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "標靶")
+        {
+            shot = true;
+        }
+    }
+}
