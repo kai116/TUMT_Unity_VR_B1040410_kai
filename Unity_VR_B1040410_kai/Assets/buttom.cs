c0af964399e3d8fc6874eb9df93f896aa511251c
kai116 <55518563+kai116@users.noreply.github.com>
2020-01-07 20:39:18 +0800

    VR輸出3

---
+++ Unity_VR_B1040410_kai/Assets/buttom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class buttom : MonoBehaviour {
+
+    public void ResetGG()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void EXITDD()
+    {
+        Application.Quit();
+    }
+}
